e7744494a3ab028ed320ba49f0cfd6ed916c926e
ethembelka <ethembelka@gmail.com>
2025-06-12 21:48:10 +0300

    .

--- fair-project/src/main/java/com/fuar/model/Event.java
+++ fair-project/src/main/java/com/fuar/model/Event.java
@@ -1,17 +1,21 @@
 package com.fuar.model;
 
-
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
-@Data
+@Getter
+@Setter
+@ToString(exclude = {"speakers", "attendees"})
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -41,22 +45,24 @@ public class Event {
 
     private String image;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
         name = "event_speakers",
         joinColumns = @JoinColumn(name = "event_id"),
         inverseJoinColumns = @JoinColumn(name = "user_id")
     )
     @Builder.Default
+    @JsonManagedReference("event-speakers")
     private Set<User> speakers = new HashSet<>();
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
         name = "event_attendees",
         joinColumns = @JoinColumn(name = "event_id"),
         inverseJoinColumns = @JoinColumn(name = "user_id")
     )
     @Builder.Default
+    @JsonManagedReference("event-attendees")
     private Set<User> attendees = new HashSet<>();
 
     private LocalDateTime createdAt;
@@ -72,5 +78,29 @@ public class Event {
     @PreUpdate
     protected void onUpdate() {
         updatedAt = LocalDateTime.now();
+    }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((title == null) ? 0 : title.hashCode());
+        result = prime * result + ((startDate == null) ? 0 : startDate.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        Event other = (Event) obj;
+        if (id == null) {
+            if (other.id != null) return false;
+        } else if (!id.equals(other.id)) {
+            return false;
+        }
+        return true;
     }
 }
